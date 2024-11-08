eab25dce91193b30149e4a07690507f95a8ba7fb
unknown <ebsung@zenithn.com>
2024-11-08 18:16:15 +0900

    change aws infomation

--- projects/2. app-gitops/files/terraform/_backend.tf
+++ projects/2. app-gitops/files/terraform/_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hands-on-cicd-pipeline-20240114021410844200000001"
+    bucket = "hands-on-cicd-pipeline-20241108083035353000000001"
     key    = "terraform.tfstate"
     region = "ap-northeast-2"
   }
